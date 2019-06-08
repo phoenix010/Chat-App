7568e1aba70b881a09fa175d2c5fa916d4d6d977
Aditya <aditya-tyagi@outlook.com>
2019-06-08 13:21:10 -0500

    location tacking is added

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,33 @@
-io();
+//client side javvascript
+const socket = io();
+
+// socket.on("countUpdated", count => {
+//   console.log("Count has been updated", count);
+// });
+
+// document.querySelector("#increment").addEventListener("click", () => {
+//   console.log("Clicked");
+//   socket.emit("increment");
+// });
+
+socket.on("message", welcome => {
+  console.log(welcome);
+});
+document.querySelector("#message-form").addEventListener("submit", e => {
+  e.preventDefault();
+  const message = e.target.elements.message.value;
+  socket.emit("sendMessage", message);
+});
+
+document.querySelector("#send-location").addEventListener("click", () => {
+  if (!navigator.geolocation) {
+    return alert("Geolocation is not supported by your browser");
+  }
+  navigator.geolocation.getCurrentPosition(position => {
+    console.log(position);
+    socket.emit("sendLocation", {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  });
+});
--- src/index.js
+++ src/index.js
@@ -15,8 +15,31 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-io.on("connection", () => {
+io.on("connection", socket => {
   console.log("new web socket connection");
+
+  socket.emit("message", "welcome");
+
+  socket.broadcast.emit("message", "A new user has joined the chat room");
+
+  socket.on("sendMessage", message => {
+    // if (message.trim() == "") {
+    //   message = "Eneter data";
+    //   socket.emit(message);
+    // }
+    io.emit("message", message);
+  });
+
+  socket.on("sendLocation", coords => {
+    io.emit(
+      "message",
+      `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
+    );
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left");
+  });
 });
 
 server.listen(port, () => {
